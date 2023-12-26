6a6a4e984ce27aa16817d214dd18df1627f96dd3
HerbertHe <Herbert.He0229@gmail.com>
2023-12-26 12:44:25 +0800

    fix :bug: : support dts

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "tsup"
 export const tsup = defineConfig({
     entry: ["src/index.ts", "src/common/index.ts", "src/types/index.ts"],
     clean: true,
+    dts: true,
     format: ["esm"],
     outDir: "dist",
 })
